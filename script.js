0b86cf0a372b0b1daa4b39909e936555d40ea853
SjoGe <156306352+SjoGe@users.noreply.github.com>
2024-10-20 23:29:38 +0300

    updates

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.getElementById('button').addEventListener('click', function() {
     alert('Long five seconds huh?');
 }, 5000);
 });
+
+document.getElementById('nameForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+
+    alert('Hello, ${firstName} ${lastName}!');
+});
